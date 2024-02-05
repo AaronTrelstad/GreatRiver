8f61c57c35fb833b17aa3263a9eba858574c0b30
AaronTrelstad <trelstadaaron@gmail.com>
2024-02-04 22:20:40 -0600

    added hashrouter

--- GreatRiverWeb/src/App.jsx
+++ GreatRiverWeb/src/App.jsx
@@ -3,13 +3,13 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Home from './pages/Home'
 import Services from './pages/services'
 import About from './pages/aboutus'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {HashRouter, Routes, Route} from 'react-router-dom'
 
 
 function App() {
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         {/* This makes it so the landing page is the home page*/}
         <Route index element={<Home />} />
@@ -17,7 +17,7 @@ function App() {
         <Route path='/services' element={<Services />}></Route>
         <Route path='/about' element={<About />}></Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
